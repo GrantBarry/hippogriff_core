c6940c9ccc05fa54b936026b992fc9375813e6cb
Grant Barry <grant.barry@gmail.com>
2024-07-07 16:16:01 +0200

    FIX: Add Enquirer foreign_key to Enquiry model

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -2,13 +2,13 @@ class Enquiry < ApplicationRecord
   belongs_to :account
   belongs_to :agent, optional: true
   belongs_to :property, optional: true
-  belongs_to :enquirer, class_name: 'Contact::Enquirer', optional: true
+  belongs_to :enquirer, class_name: 'Contact::Enquirer', optional: true, foreign_key: :contact_id
 
   validates :message, presence: true
   normalizes :message, with: ->(value) { value.strip }
 
-  validates :account_id, presence: true
-  validates :enquirer_id, presence: true
+  # validates :account_id, presence: true
+  # validates :contact_id, presence: true
 
   accepts_nested_attributes_for :enquirer
 end
--- lib/hippogriff_core/version.rb
+++ lib/hippogriff_core/version.rb
@@ -1,3 +1,3 @@
 module HippogriffCore
-  VERSION = '0.3.1'
+  VERSION = '0.3.2'
 end
