c429351c343c2a6b08dd0b1dff895b0c7a5dae67
Grant Barry <grant.barry@gmail.com>
2024-08-29 04:50:30 +0200

    ADD: Property intenal_id generation and area calculation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,32 @@
 class Property < ApplicationRecord
+  after_create_commit :generate_internal_id
+  before_save :calculated_building_area
+
   belongs_to :agent
 
   scope :latest_n, ->(n) { order(:updated_at).limit(n) }
+
+  # Building area = all areas (except land_area and hard_stand_yard_area)
+  def building_area
+    building_area_attributes
+      .map { |attr| public_send(attr) }
+      .compact_blank
+      .reduce(0.0, :+)
+  end
+
+  def total_area
+    [building_area, land_area, hard_stand_yard_area]
+      .compact_blank
+      .reduce(0.0, :+)
+  end
+
+  private
+
+  def generate_internal_id
+    update_column(:internal_id, "#{account.company_name[0..2].upcase}-#{id}") if internal_id.blank?
+  end
+
+  def calculate_building_area
+    self.calculated_building_area = building_area
+  end
 end
